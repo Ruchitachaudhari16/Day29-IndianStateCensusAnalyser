4330b1f851ae810f845d271f9f677a211804d07a
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-07-31 23:09:39 -0400

    [Ruchita]Add:Getting data-State Census using CSV file

---
+++ IndianStatesCensusAnalser/IndianStatesCensusAnalser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensusAnalser
+{
+    public class Program
+    {
+       
+  public static string path = @"C:\RFP288Batch\IndianCensusAnalyserDay29\Day29-IndianStateCensusAnalyser\IndianStatesCensusAnalser\IndianStatesCensusAnalser\Files\StateCensusData.csv";
+        static void Main(string[] args)
+        {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+            stateCensusAnalyser.ReadStateCensusData(path);
+            Console.ReadLine();
+        }
+    }
+}
+    
